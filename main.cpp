267d19576ede98e49913ebb33a9a917708b50030
CarlosErnestoEM <carlos@carlosernestoem.live>
2023-04-04 21:16:13 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string typed;
+    cout << "Bienvenido al Editor de CUPL" << endl;
+    cout << "de Carlitos jiji" << endl << endl;
+
+    cout << "El programa genera una solucion usando la instruccion TABLE" << endl;
+    cout << "Enter para continuar"; cin.ignore();
+}
