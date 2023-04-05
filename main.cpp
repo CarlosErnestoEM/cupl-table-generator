deaad0072e66635c475d22a9130bf18ced89392f
CarlosErnestoEM <carlos@carlosernestoem.live>
2023-04-04 21:54:26 -0600

    Now create the file with a custom name

--- main.cpp
+++ main.cpp
@@ -14,14 +14,32 @@ int main(){
     cout << "Enter para continuar"; cin.ignore();
 
 
-
-    pld.close();
     //Save file with custom name
     cout << "Da el nombre al archivo sin espacios: ";
     cin >> typed;
     typed = typed + ".pld";
     char* fileName = new char[typed.length() + 1];
     strcpy(fileName, typed.c_str());
+
+    //Creating file
+    pld << "Name    " << typed << " ;"<< endl;
+    pld << "PartNo  " << "00" << " ;"<< endl;
+    pld << "Date  " << "09/03/2022" << " ;"<< endl;
+    pld << "Revision  " << "01" << " ;"<< endl;
+    pld << "Designer  " << "Estrada Carlos" << " ;"<< endl;
+    pld << "Company  " << "ESIME" << " ;"<< endl;
+    pld << "Assembly  " << "None" << " ;"<< endl;
+    pld << "Location  " << " " << " ;"<< endl;
+    pld << "Device  " << "G22V10" << " ;"<< endl;
+    pld << "PIN [1..4] = [E0..3] ;"<< endl;
+    pld << "PIN [23..16] = [S0..7] ;"<< endl << endl;
+    pld << "FIELD entrada = [E0..3] ;"<< endl;
+    pld << "FIELD salida = [S0..7] ;"<< endl;
+    pld << "TABLE entrada => salida {"<< endl;
+    //Create table using an array
+
+    pld << "}";
+    pld.close();
 
     if (rename("file.pld", fileName) != 0)
 		perror("Error renaming file");
